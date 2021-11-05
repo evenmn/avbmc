3cdb013e03e1bfde7b63927129488b07015d7ae7
evenmn <evenmn@fys.uio.no>
2021-11-05 11:00:27 +0100

    Implemented parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,28 +1,63 @@
 #include "parser.h"
 
+vector<string> split(const string s)
+{
+    /* Split wtring by whitespace
+     */
+    std::stringstream ss(s);
+    std::istream_iterator<std::string> begin(ss);
+    std::istream_iterator<std::string> end;
+    std::vector<std::string> vstrings(begin, end);
+    return vstrings;
+}
 
-void parser(int argc, char** argv[])
+
+void parser(int argc, char** argv)
 {
     /* Parser main function
      */
     if(argc >= 2){
-        Box box();
-        string cmd_cat = argv[1];
-        if(cmd_cat == "set"){
-            set(box, argc, argv[]);
-        }
-        else if(cmd_cat == "add"){
-            add(box, argc, argv[]);
-        }
-        else if(cmd_cat == "take"){
-            take(box, argc, argv[]);
-        }
-        else if(cmd_cat == "run"){
-            run(box, argc, argv[]);
+        string filename = argv[1];
+        ifstream f(filename);
+
+        if (f.is_open()){ 
+            Box box("");
+            string line;
+            while (getline(f, line))
+            {
+                if (line.empty()) {
+                    /* Continue if line is blank */
+                    continue;
+                }
+                else if(line.rfind("#", 0) == 0){
+                    /* Continue if line is commented out */
+                    continue;
+                }
+                else{
+                    vector<string> splitted = split(line);
+                    int argc = splitted.size();
+                    string cmd_cat = splitted[0];
+                    if(cmd_cat == "set"){
+                        set(box, splitted, argc);
+                    }
+                    else if(cmd_cat == "add"){
+                        add(box, splitted, argc);
+                    }
+                    else if(cmd_cat == "take"){
+                        take(box, splitted, argc);
+                    }
+                    else if(cmd_cat == "run"){
+                        run(box, splitted, argc);
+                    }
+                    else{
+                        cout << "There is no category '" + cmd_cat + "'! Aborting." << endl;
+                        exit(0);
+                    }
+                }
+            }
         }
         else{
-            cout << "There is no category '" + cmd_cat + "'! Aborting." << endl;
-            exit(0);
+            cout << "Could not open the file!" << endl;
         }
     }
     else{
@@ -32,68 +67,66 @@ void parser(int argc, char** argv[])
     }
 }
 
-
-void set(Box& box, int argc, char** argv[])
+void set(Box& box, const vector<string> splitted, const int argc)
 {
-    /*
-     */
     
-    assert(argc > 2);
-    string keyword = argv[2];
+    assert(argc > 1);
+    string keyword = splitted[1];
     if(keyword == "temp"){
-        box.set_temp(stod(argv[3]));
+        box.set_temp(stod(splitted[2]));
     }
     else if(keyword == "chempot"){
-        box.set_chempot(stod(argv[3]));
+        box.set_chempot(stod(splitted[2]));
     }
     else if(keyword == "forcefield"){
-        assert(argc >= 5);
-        string forcefield = argv[3];
-        string paramfile = argv[4];
+        assert(argc > 2);
+        string forcefield = splitted[2];
+        string paramfile = splitted[3];
         if(forcefield == "lennardjones"){
             box.set_forcefield(new LennardJones(&box, paramfile));
         }
-        else if(forcefield == "vashishta"){
-            box.set_forcefield(new Vashishta(&box, paramfile));
-        }
+        //else if(forcefield == "vashishta"){
+        //    box.set_forcefield(new Vashishta(&box, paramfile));
+        //}
         else{
-            cout << "Forcefield '" + forcefield + "' is not known! Aborting." << end;
+            cout << "Forcefield '" + forcefield + "' is not known! Aborting." << endl;
             exit(0);
         }
     }
     else if(keyword == "integrator"){
-        string integrator = argv[3];
-        if(argc == 4){
+        string integrator = splitted[2];
+        if(argc == 3){
             if(integrator == "euler"){
                 box.set_integrator(new Euler(&box));
             }
-            else if(integrator == "eulercromer"){
-                box.set_integrator(new EulerCromer(&box));
-            }
-            else if(integrator == "velocityverlet"){
-                box.set_integrator(new VelocityVerlet(&box));
-            }
-            else if(integrator == "rungekutta4"){
-                box.set_integrator(new RungeKutta4(&box));
-            }
+            //else if(integrator == "eulercromer"){
+            //    box.set_integrator(new EulerCromer(&box));
+            //}
+            //else if(integrator == "velocityverlet"){
+            //    box.set_integrator(new VelocityVerlet(&box));
+            //}
+            //else if(integrator == "rungekutta4"){
+            //    box.set_integrator(new RungeKutta4(&box));
+            //}
             else{
                 cout << "Integrator '" + integrator + "' is not known! Aborting." << endl;
                 exit(0);
             }
-        else if(argc >= 5){
-            double dt = stod(argv[4]);
+        }
+        else if(argc >= 4){
+            double dt = stod(splitted[3]);
             if(integrator == "euler"){
                 box.set_integrator(new Euler(&box, dt));
             }
-            else if(integrator == "eulercromer"){
-                box.set_integrator(new EulerCromer(&box, dt));
-            }
-            else if(integrator == "velocityverlet"){
-                box.set_integrator(new VelocityVerlet(&box, dt));
-            }
-            else if(integrator == "rungekutta4"){
-                box.set_integrator(new RungeKutta4(&box, dt));
-            }
+            //else if(integrator == "eulercromer"){
+            //    box.set_integrator(new EulerCromer(&box, dt));
+            //}
+            //else if(integrator == "velocityverlet"){
+            //    box.set_integrator(new VelocityVerlet(&box, dt));
+            //}
+            //else if(integrator == "rungekutta4"){
+            //    box.set_integrator(new RungeKutta4(&box, dt));
+            //}
             else{
                 cout << "Integrator '" + integrator + "' is not known! Aborting." << endl;
                 exit(0);
@@ -103,6 +136,137 @@ void set(Box& box, int argc, char** argv[])
             cout << "Integrator does not have a sufficient number of arguments! Aborting." << endl;
             exit(0);
         }
+    }
+    else if(keyword == "sampler"){
+        string sampler = splitted[2];
+        if(sampler == "metropolis"){
+            box.set_sampler(new Metropolis(&box));
+        }
+        //else if(sampler == "umbrella"){
+        //    box.set_sampler(new Umbrella(&box));
+        //}
+        else {
+            cout << "Sampler '" + sampler + "' is not known! Aborting." << endl;
+            exit(0);
+        }
+    }
+    else if(keyword == "rng"){
+        string rng = splitted[2];
+        if(rng == "mersennetwister"){
+            box.set_rng(new MersenneTwister());
+        }
+        else{
+            cout << "Random number generator '" + rng + "' is not known! Aborting." << endl;
+        }
+    }
+    else if(keyword == "boundary"){
+        assert(argc > 3);
+        string boundary = splitted[2];
+        if(boundary == "stillinger"){
+            double rc = stod(splitted[3]);
+            box.set_boundary(new Stillinger(&box, rc));
+        }
+        else if(boundary == "fixed"){
+            if(argc == 4){
+                // one box length, indicating one dimension
+                double lx = stod(splitted[3]);
+                box.set_boundary(new Fixed(&box, lx));
+            }
+            else if(argc == 5){
+                // two box lengths, indicating two dimensions
+                double lx = stod(splitted[3]);
+                double ly = stod(splitted[4]);
+                box.set_boundary(new Fixed(&box, lx, ly));
+            }
+            else if(argc == 6){
+                double lx = stod(splitted[3]);
+                double ly = stod(splitted[4]);
+                double lz = stod(splitted[5]);
+                box.set_boundary(new Fixed(&box, lx, ly, lz));
+            }
+        }
+    }
+    else{
+        cout << "Keyword '" + keyword + "' is not known! Aborting." << endl;
+        exit(0);
+    }
+}
+
+
+void add(Box& box, const vector<string> splitted, const int argc)
+{
+    assert(argc > 1);
+    string keyword = splitted[1];
+
+    if(keyword == "move"){
+        assert(argc > 3);
+        string move = splitted[2];
+        double prob = stod(splitted[3]);
+        if(move == "trans"){
+            if(argc == 4){
+                box.add_move(new Trans(&box), prob);
+            }
+            else if(argc >= 5){
+                double dx = stod(splitted[4]);
+                box.add_move(new Trans(&box, dx), prob);
+            }
+        }
+        else if(move == "transmh"){
+            if(argc == 4){
+                box.add_move(new TransMH(&box), prob);
+            }
+            else if(argc == 5){
+                double dx = stod(splitted[4]);
+                box.add_move(new TransMH(&box, dx), prob);
+            }
+            else if(argc >= 6){
+                double dx = stod(splitted[4]);
+                double Ddt = stod(splitted[5]);
+                box.add_move(new TransMH(&box, dx, Ddt), prob);
+            }
+        }
+        else{
+            //
+            cout << "Hey" << endl;
+        }
+    }
+    else if(keyword == "particle"){
+        assert(argc > 3);
+        // Need to figure out the best way to treat particles
+    }
+    else{
+        cout << "Keyword '" + keyword + "' is not known! Aborting." << endl;
+        exit(0);
+    }
+}
+
+void take(Box& box, const vector<string> splitted, const int argc)
+{
+    assert(argc > 1);
+    string keyword = splitted[1];
+    if(keyword == "snapshot"){
+        assert(argc > 2);
+        string filename = splitted[2];
+        box.snapshot(filename);
+    }
+    else{
+        cout << "Keyword '" + keyword + "' is not known! Aborting." << endl;
+        exit(0);
+    }
+}
+
+void run(Box& box, const vector<string> splitted, const int argc)
+{
+    assert(argc > 2);
+    string keyword = splitted[1];
+    int nsteps = stod(splitted[2]);
+    if(keyword == "mc"){
+        assert(argc > 3);
+        int nmoves = stod(splitted[3]);
+        box.run_mc(nsteps, nmoves);
+    }
+    else if(keyword == "md"){
+        box.run_md(nsteps);
     }
     else{
         cout << "Keyword '" + keyword + "' is not known! Aborting." << endl;
--- src/parser.h
+++ src/parser.h
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <cassert>
 #include <armadillo>
+#include <iterator>
+#include <vector>
 
 #include "box.h"
+
+#include "integrator/integrator.h"
+#include "integrator/euler.h"
+
+#include "forcefield/forcefield.h"
+#include "forcefield/lennardjones.h"
+
+#include "sampler/sampler.h"
+#include "sampler/metropolis.h"
+
+#include "moves/moves.h"
+#include "moves/trans.h"
+#include "moves/transmh.h"
+#include "moves/avbmcout.h"
+
+#include "boundary/boundary.h"
+#include "boundary/fixed.h"
+#include "boundary/stillinger.h"
+
 
 using namespace std;
 using namespace arma;
 
 
-void parser(int argc, char** argv[]);
-void set(Box& box, string keyword);
-void add(Box& box, string keyword);
-void take(Box& box, string keyword);
-void run(Box& box, string keyword);
+void parser(int argc, char** argv);
+void set(Box& box, const vector<string> splitted, const int argc);
+void add(Box& box, const vector<string> splitted, const int argc);
+void take(Box& box, const vector<string> splitted, const int argc);
+void run(Box& box, const vector<string> splitted, const int argc);
+vector<string> split(const string s);
