4882deb19851c846a3f8d1f6638960eb2fb3eeb9
evenmn <evenmn@fys.uio.no>
2021-11-16 14:51:29 +0100

    Added main

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <string>
-#include <armadillo>
 #include <vector>
 
 #include "box.h"
 #include "init_position.h"
 #include "forcefield/lennardjones.h"
-#include "integrator/euler.h"
+//#include "integrator/euler.h"
 #include "sampler/metropolis.h"
 #include "moves/trans.h"
-#include "moves/transmh.h"
+//#include "moves/transmh.h"
 #include "moves/avbmcout.h"
 
 using namespace std;
-using namespace arma;
 
 
 int main()
@@ -22,36 +20,21 @@ int main()
     Box box("simulation", 300., 0.);
 
     // initialize particles
-    double mass = 1.;
-    mat position = fcc(3, 5., 3);
-    mat velocity = randn(position.n_rows, position.n_cols);
-    string chem = "Ar";
-    box.add_particles(chem, position, velocity);
-    box.set_mass(chem, mass);
+    Particle *particle;
+    particle->label = "Ar";
+    particle->r = {0., 0., 0.};
+    box.add_particle(particle);
+    box.set_mass("Ar", 1.);
 
     box.snapshot("initial.xyz");
-
-    //box.dump(1, "dump.xyz", "x", "y", "z");
-
-    // relax with molecular dynamics simulation
-    box.set_integrator(new Euler(&box));
-    box.set_forcefield(new LennardJones(&box, ".in"));
-    vector<string> outputs = {"Step", "PotEng", "AcceptanceRatio"};
-    box.set_thermo(2, "md.log", outputs);
-    outputs = {"xyz"};
-    box.set_dump(100, "mc.xyz", outputs);
-    //box.run_md(100);
-    //box.snapshot("after_md.xyz");
     
     // run Monte Carlo
     box.set_sampler(new Metropolis(&box));
-    box.add_move(new Trans(&box, 0.1), 0.5);
-    box.add_move(new TransMH(&box, 0.01, 0.01), 0.5);
+    box.add_move(new Trans(&box, 0.1), 1.0);
     //box.add_move(new AVBMCOut(&box, 0.5, 2.), 0.25);
     //box.thermo(1, "mc.log", "step", "poteng", "acc_ratio");
-    //box.run_mc(steps=1000, out="log");
     box.run_mc(1000, 100);
-    box.snapshot("final.xyz");
+    //box.snapshot("final.xyz");
     
     return 0;
 }
