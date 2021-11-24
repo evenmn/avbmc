030581377d8e7144adffdbc559e431334af5e0f3
evenmn <evenmn@fys.uio.no>
2021-11-24 13:59:23 +0100

    Made header more compact

--- src/forcefield/lennardjones.h
+++ src/forcefield/lennardjones.h
@@ -17,7 +17,7 @@ public:
     double comp_energy_par(const std::vector<class Particle *> particles, const int i);
     //double update_force_par(const mat positions, const int i);
     //double update_force_all();
-    //~LennardJones();
+    ~LennardJones();
 
     //std::vector<int> build_neigh_list(const mat positions, const int i);
     //void build_neigh_lists();
@@ -26,11 +26,8 @@ private:
     int nline;
 
     // vectors to store raw data from param file
-    std::vector<std::string> label1_vec;
-    std::vector<std::string> label2_vec;
-    std::vector<double> sigma_vec;
-    std::vector<double> epsilon_vec;
-    std::vector<double> rc_vec;
+    std::vector<std::string> label1_vec, label2_vec;
+    std::vector<double> sigma_vec, epsilon_vec, rc_vec;
 
     // matrices to store sorted params
     double **sigma_mat, **epsilon_mat, **rc_sqrd_mat;
