e506a15c64476d8c87e7461d7693964db1fe081f
evenmn <evenmn@fys.uio.no>
2022-01-19 21:32:30 +0100

    Better Metropolis comments

--- src/sampler/metropolis.cpp
+++ src/sampler/metropolis.cpp
@@ -1,10 +1,20 @@
+/* ----------------------------------------------------------------------------
+------------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------------
+   Metropolis sampling, as originally proposed by Metropolis, 
+   Rosenbluth, Rosenbluth, Teller and Teller (1954). The acceptance
+   of the move is decided by the move's 'accept'-method, as the
+   acceptance criterion is move-specific
+------------------------------------------------------------------------------- */
+
 #include "metropolis.h"
 #include "../system.h"
 
 
-/* ---------------------------------------
+/* ----------------------------------------------------------------------------
    Metropolis constructor
------------------------------------------- */
+------------------------------------------------------------------------------- */
 
 Metropolis::Metropolis(System* system_in)
     : Sampler(system_in) 
@@ -13,11 +23,11 @@ Metropolis::Metropolis(System* system_in)
 }
 
 
-/* ---------------------------------------
+/* ----------------------------------------------------------------------------
    Weight function (umbrella) to be added
    to energy for biased moves. For 
    Metropolis, this should always be zero
------------------------------------------- */
+------------------------------------------------------------------------------- */
 
 double Metropolis::w(const int /*npar*/)
 {
