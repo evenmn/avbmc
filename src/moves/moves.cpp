b46441c12466e3d4e742e4f1213082ee6b2afdc1
evenmn <evenmn@mn.uio.no>
2022-01-31 15:34:12 +0100

    Fixed rotation function

--- src/moves/moves.cpp
+++ src/moves/moves.cpp
@@ -21,9 +21,7 @@ Moves::Moves(System* system_in)
 
 
 /* ----------------------------------------------------------------------------
-   Rotate molecule by a random angle in all dimensions. Might send in positions
-   as a reference in the future, even though we still may need to copy the
-   positions
+   Rotate molecule around the center of mass atom by a random angle. 
 ------------------------------------------------------------------------------- */
 
 std::vector<Particle> Moves::rotate_molecule(std::vector<Particle> particles)
@@ -42,9 +40,38 @@ std::vector<Particle> Moves::rotate_molecule(std::vector<Particle> particles)
         }
     }
     else{
-        double anglex = 2 * pi * rng->next_double();
-        double angley = 2 * pi * rng->next_double();
-        double anglez = 2 * pi * rng->next_double();
+        double anglea = 2 * pi * rng->next_double();
+        double cosa = std::cos(anglea);
+        double sina = std::sin(anglea);
+
+        double angleb = 2 * pi * rng->next_double();
+        double cosb = std::cos(angleb);
+        double sinb = std::sin(angleb);
+
+        double anglec = 2 * pi * rng->next_double();
+        double cosc = std::cos(anglec);
+        double sinc = std::sin(anglec);
+
+        double Axx = cosa * cosb;
+        double Axy = cosa * sinb * sinc - sina * cosc;
+        double Axz = cosa * sinb * cosc + sina * sinc;
+
+        double Ayx = sina * cosb;
+        double Ayy = sina * sinb * sinc + cosa * cosc;
+        double Ayz = sina * sinb * cosc - cosa * sinc;
+
+        double Azx = -sinb;
+        double Azy = cosb * sinc;
+        double Azz = cosb * cosc;
+
+        for (Particle &particle : particles) {
+            std::valarray<double> r = particle.r;
+            particle.r[0] = Axx*r[0] + Axy*r[1] + Axz*r[2];
+            particle.r[1] = Ayx*r[0] + Ayy*r[1] + Ayz*r[2];
+            particle.r[2] = Azx*r[0] + Azy*r[1] + Azz*r[2];
+        }
+
+        /*
         for(Particle &particle : particles){
             std::valarray<double> r = particle.r;
             particle.r = {
@@ -59,6 +86,7 @@ std::vector<Particle> Moves::rotate_molecule(std::vector<Particle> particles)
                     + r[1] * std::sin(anglex)
             };
         }
+        */
     }
     return particles;
 }
@@ -70,8 +98,10 @@ std::vector<Particle> Moves::rotate_molecule(std::vector<Particle> particles)
 
 double Moves::norm(std::valarray<double> array)
 {
-    double normsq = 0.;
-    for (unsigned int i=0; i < array.size(); i++)
+    double normsq;
+    unsigned int i;
+    normsq = 0.;
+    for (i=0; i < array.size(); i++)
     {
         normsq += array[i] * array[i];
     }
@@ -80,23 +110,25 @@ double Moves::norm(std::valarray<double> array)
 
 
 /* ----------------------------------------------------------------------------
-   Build neighbor list of particle 'i' with maximum neighbor
-   distance squared 'rsq'
+   Build neighbor list of particle 'i' with maximum neighbor distance squared
+   'rsq'
 ------------------------------------------------------------------------------- */
 
 std::vector<int> Moves::build_neigh_list(std::vector<Particle> particles, const int i, const double rsq)
 {
-    unsigned int npar = particles.size();
     double rijsq;
+    unsigned int npar, j;
+    
+    npar = particles.size();
     std::valarray<double> ri = particles[i].r;
     std::vector<int> neigh_list;
-    for(unsigned int j=0; j<i; j++){
+    for(j=0; j<i; j++){
         rijsq = std::pow(particles[j].r - ri, 2).sum();
         if(rijsq < rsq){
             neigh_list.push_back(j);
         }
     }
-    for(unsigned int j=i+1; j<npar; j++){
+    for(j=i+1; j<npar; j++){
         rijsq = std::pow(particles[j].r - ri, 2).sum();
         if(rijsq < rsq){
             neigh_list.push_back(j);
@@ -110,13 +142,14 @@ std::vector<int> Moves::build_neigh_list(std::vector<Particle> particles, const 
    Check if particles match molecule type recursively.
 ------------------------------------------------------------------------------- */
 
-void Moves::check_neighbors(const int k, std::vector<Particle> molecule, unsigned int elm_count,
-                            std::vector<int> &elm_idx, std::vector<Particle> particles, double rc) {
-    if (elm_idx.size() < molecule.size()) {  // ensure that recursion stops when molecule has reaced correct size
-        if (particles[k].label == molecule[elm_count].label) {
-            elm_idx.push_back(k);
+void Moves::check_neighbors(const int i, std::vector<Particle> molecule,
+                            unsigned int elm_count, std::vector<int> &elm_idx,
+                            std::vector<Particle> particles, double rc) {
+    if (elm_idx.size() < molecule.size()) {  
+        if (particles[i].label == molecule[elm_count].label) {
+            elm_idx.push_back(i);
             elm_count ++;
-            std::vector<int> neigh_list = build_neigh_list(particles, k, rc);
+            std::vector<int> neigh_list = build_neigh_list(particles, i, rc);
             for (int neigh : neigh_list) {
                 check_neighbors(neigh, molecule, elm_count, elm_idx, particles, rc);
             }
@@ -126,7 +159,7 @@ void Moves::check_neighbors(const int k, std::vector<Particle> molecule, unsigne
 
 
 /* ----------------------------------------------------------------------------
-   Detect molecule of the same types as 'molecule'. randomly by picking a random 
+   Detect molecule of the same types as 'molecule' randomly by picking a random 
    atom among the elements and checking the neighbor list.
    Returning a list of atom ids if molecule is detected
 ------------------------------------------------------------------------------- */
@@ -140,8 +173,8 @@ std::vector<int> Moves::detect_molecule(std::vector<Particle> particles,
     while (count < particles.size() && !detected)
     {
         elm_idx.clear();
-        int k = rng->next_int(particles.size());     // pick initial particle
-        check_neighbors(k, molecule, 0, elm_idx, particles, rc);
+        int i = rng->next_int(particles.size());     // pick initial particle
+        check_neighbors(i, molecule, 0, elm_idx, particles, rc);
         if (elm_idx.size() == molecule.size()) {
             detected = true;
         }
@@ -151,6 +184,9 @@ std::vector<int> Moves::detect_molecule(std::vector<Particle> particles,
     {
         elm_idx.clear();
     }
+    //for (int idx : elm_idx) {
+    //    std::cout << particles[idx].label << " ";
+    //}
     return elm_idx;
 }
 
