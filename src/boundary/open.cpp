21a7bc11b8c4ac8b40a8be6fe25b919b02d88006
evenmn <evenmn@mn.uio.no>
2022-02-25 16:16:52 +0100

    Implemented open boundaries

---
+++ src/boundary/open.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <valarray>
+
+#include "../box.h"
+#include "../particle.h"
+#include "open.h"
+
+
+/* ----------------------------------------------------------------------------
+   Open boundary constructor
+------------------------------------------------------------------------------- */
+
+Open::Open(Box* box_in)
+    : Boundary(box_in)
+{}
