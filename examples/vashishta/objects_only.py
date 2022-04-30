a592250981b9fae16e4f5344d11e3145067ccab7
evenmn <evenmn@mn.uio.no>
2022-04-30 18:22:12 +0200

    Added vashishta object example

---
+++ examples/vashishta/objects_only.py
@@ -0,0 +1,18 @@
+import avbmc as mc
+
+system = mc.System()
+box = mc.Box(system)
+system.add_box(box)
+
+forcefield = mc.Vashishta(box, "H2O.wang.vashishta")
+box.set_forcefield(forcefield)
+
+box.read_particles("water.xyz")
+
+move = mc.Trans(system, box, 0.1)
+system.add_move(move)
+
+box.set_dump(1000, "mc.xyz", ["x", "y", "z"])
+box.set_thermo(1000, "mc.log", ["step", "atoms", "poteng"])
+
+system.run_mc(10000000, 1)
