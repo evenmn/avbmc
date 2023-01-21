3572431b2e20ee719109a0dfa09a8288f732b746
evenmn <evenmn@fys.uio.no>
2023-01-21 19:49:01 +0100

    Removed parser files from setup

--- setup.py
+++ setup.py
@@ -18,8 +18,6 @@ with open("README.md", "r") as fh:
 source_files = glob('src/*.cpp')
 source_files.extend(glob('src/**/*.cpp'))
 source_files.remove('src/main.cpp')
-source_files.remove('src/main_parser.cpp')
-#source_files.remove('src/parser.cpp')
 source_files.append('python_wrapper/wrap.cpp')
 print(source_files)
 
